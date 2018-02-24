011b590c087b8a2cff922b89b8e37f76ecae4ced
qh08 <qihongfrank@foxmail.com>
2018-02-24 22:46:02 +0800

    use async.mapLimit

--- src/spider/user.js
+++ src/spider/user.js
@@ -1,24 +1,35 @@
 const superagent = require('superagent');
 const cheerio = require('cheerio');
-const player = require('../config/player');
+const async = require('async');
+
+const players = require('../config/player');
 
 const url_prefix = 'https://pubg.op.gg/user/';
 const url_server = 'as';
 
-player.forEach(function (value, index) {
-    const url = url_prefix + value + '?server=' + url_server;
+async.mapLimit(players, 2, function (player) {
+    const url = url_prefix + player;
+    // const url = 'https://www.baidu.com';
     superagent
         .get(url)
+        .query({
+            server : 'as'
+        })
         .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8')
-        .set('cookie','_ga=GA1.2.1217097403.1518095147; _gid=GA1.2.1025910423.1519391337; _referrer=user,; wP_v=10b6dacbebd3sIre7qgh8mrA7ImhhIEarS6hY_EnEguB_SeedSVoTgRnx6vwOMR; XSRF-TOKEN=eyJpdiI6IldvdWNlWjM3MUhIKzBrRHlYeTB5T2c9PSIsInZhbHVlIjoieDJMdXFTSkJsb2w5RUgyZVBYbVNtRHZuK3Ezald2NFk0akg3NXJRUlErVjl2MVFidWpzM0tlZXJMSTBIZ2NsRnhRNGduRzUrd1JBUjBNQnJvZFNIdGc9PSIsIm1hYyI6IjgwODFhZDIwMjZiZWVkNTFmODdlMWIwZWE1NGU3YzkwM2MzNDdiYmFhZTIzODc3ZmE3YmYyMDliNzFmOTlkMzEifQ%3D%3D; pubg_session=eyJpdiI6IitPZjFkb1ZKYThUT2psUURraWYzS0E9PSIsInZhbHVlIjoiTUtrMVM5ZUtZNE1ldWZBaThqeDlrMEM2anBKSDF0TVhIRnlkeGFWQWhQdk5TcE5Uc3grREdXTWlVeTVJdXpXczVDWE00UE1iNkVxaXBKMVZEWFpFZlE9PSIsIm1hYyI6IjFmNmU2NGYyNDZmMmYyYzFkODlhODg4NGFkZTM3NDRlYWQzYzdkNmU0ODE3ZTE0OTE1Y2E3NDk1OTM4NzRkMjcifQ%3D%3D;')
-        .end(function (err, res) {
-
-            if (err) {
-                console.log('error');
-            }
-
+        .set('cookie', '_ga=GA1.2.1217097403.1518095147; _gid=GA1.2.1025910423.1519391337; _referrer=user,; XSRF-TOKEN=eyJpdiI6IlVSR05vbkdEZlpNRTBodGZIN05WbWc9PSIsInZhbHVlIjoiRlJHWDA1SVByOEUxODBrMGNJMUZhelpkT2U1YVRwTjdRSTErQUtMeXEwM2gzY0tzZlFTcXFmNHZrdFBLQVwvOGlZNXIyTFpUVm96bGt3d0FIMll5aTdRPT0iLCJtYWMiOiIxOWYxMjczOTdmZGE3NzNkNmY5ODllY2Q5NWM2YWY0NmNjMDhkOGNkZWM5MzNmM2FhZTlmN2U3MGZjZGQ1ZjVjIn0%3D; pubg_session=eyJpdiI6IkRTNUluREZjWkllZkdEaktDUmQ3bmc9PSIsInZhbHVlIjoiQ3J5ODR4UlFWVzgzV25ibXVuN3RxVExRbUpWcmhDQnNZblNGXC8zTWVmazVNMEFvNEVvN2ROcFhIXC9XT1dBWnBJampOWEdkZTF1aUlqY1pHb0J3VURwUT09IiwibWFjIjoiOTNjZGIxYmVkMTQ3ZjE4MjgyNTk4NDUxMTI1MDQzMTVjN2JmZTk3OTllNmRjM2MwMDkzYjc3NWRlZjBiZWIxOSJ9; recent-searches=Take-you-fly,flygepi,17_shou,kunbufenzi,faker111,feiker1111,frankknnaarf')
+        .set('upgrade-insecure-requests', 1)
+        .then(function (res) {
             const $ = cheerio.load(res.text) || {};
-            const user_id = $('#userNickname').prop('data-user_id') || 'err';
-            console.log(value + ':' + user_id);
+            console.log($.html());
+            // const user_id = $('#userNickname').prop('data-user_id') || 'err';
+            // // return player + ':' + user_id;
+            // console.log(player + ':' + user_id);
+        })
+        .catch(function (err) {
+            console.log('error');
+            console.log(err.message);
         });
+}, function (err, result) {
+    if (err) throw err;
+    console.dir(result);
 });
